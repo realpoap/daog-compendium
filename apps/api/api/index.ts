d6cfa4cf5a16be5d033abad6e43bfcf911e8f2a1
realpoap <alapetite.mathieu@gmail.com>
2024-12-07 17:43:33 -0500

    default vercel

--- apps/api/api/index.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-export type { AppRouter } from '../src/router';
-export type { RouterInput, RouterOutput } from '../src/trpc';
-
-export const prisma = new PrismaClient();
