15b2e36ff94ad947c4aad8e2fe663e5124e290c1
realpoap <alapetite.mathieu@gmail.com>
2024-12-13 15:36:32 -0500

    deployment workflow

--- apps/web/src/routes/rules/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/rules/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/rules/"!</div>
-}
