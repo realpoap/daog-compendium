ee749a7e64a9576f6fa7381b895af3dd49deef36
realpoap <alapetite.mathieu@gmail.com>
2025-01-18 16:00:45 -0500

    🐛 FIX: build errors

--- apps/web/src/components/Bestiary/NewCreature/FormSteps/LootStep.tsx
+++ apps/web/src/components/Bestiary/NewCreature/FormSteps/LootStep.tsx
@@ -1,63 +1,55 @@
 import { ActionButton } from '@/components/Buttons';
-import { trpc } from '@/utils/trpc';
-import { CreatureComponent } from '@api/lib/ZodComponent';
 import { NewCreature } from '@api/lib/ZodCreature';
-import { CreatureItem } from '@api/lib/ZodItem';
-import { useEffect, useState } from 'react';
-import { UseFormSetValue } from 'react-hook-form';
 import { RiArrowDropLeftLine, RiArrowDropRightLine } from 'rocketicons/ri';
-import { Field } from '../../../RHFComponents';
-import { ComponentsTags } from '../../utils/ComponentsTags';
-import { ItemsTags } from '../../utils/ItemsTag';
 
 const LootStep = ({
 	handlePrevious,
 	handleNext,
-	setValue,
+	//setValue,
 }: {
 	handlePrevious: (step: number) => void;
 	handleNext: (
 		inputs: (keyof NewCreature)[],
 		nextStep: number,
 	) => Promise<void>;
-	setValue: UseFormSetValue<NewCreature>;
+	//setValue: UseFormSetValue<NewCreature>;
 }) => {
-	const componentsList = trpc.components.getAll.useQuery();
-	const itemsList = trpc.items.getAll.useQuery();
-	const [components, setComponents] = useState<string[]>([]);
-	const [items, setItems] = useState<string[]>([]);
+	// const componentsList = trpc.components.getAll.useQuery();
+	// const itemsList = trpc.items.getAll.useQuery();
+	// const [components, setComponents] = useState<string[]>([]);
+	// const [items, setItems] = useState<CreatureItem[]>([]);
 
-	useEffect(() => {
-		if (componentsList.data) {
-			const list = componentsList.data;
-			const compObjects = [] as CreatureComponent[];
-			components?.map(att => {
-				const compObject = list.find(el => el.name === att);
-				if (compObject) {
-					const { id, ...rest } = compObject;
-					compObjects.push(rest);
-				}
-			});
-			console.log(compObjects);
-			setValue('scavenge', compObjects as CreatureComponent[]);
-		}
-	}, [components]);
+	// useEffect(() => {
+	// 	if (componentsList.data) {
+	// 		const list = componentsList.data;
+	// 		const compObjects = [] as CreatureComponent[];
+	// 		components?.map(att => {
+	// 			const compObject = list.find(el => el.name === att);
+	// 			if (compObject) {
+	// 				const { id, ...rest } = compObject;
+	// 				compObjects.push(rest);
+	// 			}
+	// 		});
+	// 		console.log(compObjects);
+	// 		setValue('scavenge', compObjects as CreatureComponent[]);
+	// 	}
+	// }, [components]);
 
-	useEffect(() => {
-		if (itemsList.data) {
-			const list = itemsList.data;
-			const itemObjects = [] as CreatureItem[];
-			items?.map(att => {
-				const itemObject = list.find(el => el.name === att);
-				if (itemObject) {
-					const { id, ...rest } = itemObject;
-					itemObjects.push(rest);
-				}
-			});
-			console.log(itemObjects);
-			setValue('loot', itemObjects as CreatureItem[]);
-		}
-	}, [items]);
+	// useEffect(() => {
+	// 	if (itemsList.data) {
+	// 		const list = itemsList.data;
+	// 		const itemObjects = [] as CreatureItem[];
+	// 		items?.map(att => {
+	// 			const itemObject = list.find(el => el.name.some(att));
+	// 			if (itemObject) {
+	// 				const { id, ...rest } = itemObject;
+	// 				itemObjects.push(rest);
+	// 			}
+	// 		});
+	// 		console.log(itemObjects);
+	// 		setValue('loot', itemObjects as CreatureItem[]);
+	// 	}
+	// }, [items]);
 
 	return (
 		<div className='flex w-full flex-col items-center justify-center'>
@@ -65,14 +57,14 @@ const LootStep = ({
 				<h3 className='font-grenze text-left text-4xl text-purple-400'>
 					Equipment
 				</h3>
-				<div className='font-noto italic text-stone-500'>
+				{/* <div className='font-noto italic text-stone-500'>
 					<p>
 						Objects and trinkets worn by the creature, either they are used or
 						not :
 					</p>
 					{items.length !== 0 ? (
 						<div className='mt-4 h-4 w-full p-2'>
-							{items?.map(i => <li key={i}>{i}</li>)}
+							{items?.map(i => <li key={i.id}>{i.id}</li>)}
 						</div>
 					) : (
 						<div className='skeleton mt-4 h-4 w-full p-2 dark:bg-stone-700'></div>
@@ -119,7 +111,7 @@ const LootStep = ({
 					) : (
 						<div className='skeleton h-10 w-full dark:bg-stone-700'></div>
 					)}
-				</Field>
+				</Field> */}
 			</div>
 			<div className='flex w-full flex-row items-center justify-center gap-4'>
 				<ActionButton
