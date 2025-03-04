0eb5fe08ae6ac13443fac72bdfa63a81b20ee706
realpoap <alapetite.mathieu@gmail.com>
2025-03-03 20:00:31 -0500

    🐛 FIX: basepath

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -30,7 +30,7 @@ export function App() {
 				<Toaster />
 				<RouterProvider
 					router={router}
-					basepath='/daog-compendium'
+					basepath='/daog-compendium/'
 				/>
 			</AuthContextProvider>
 		</TrpcWrapper>
