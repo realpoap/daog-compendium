bd425143d3ca2bbbfdf8864f019ebdd5480c406c
realpoap <alapetite.mathieu@gmail.com>
2025-03-29 16:07:08 -0400

    👌 IMPROVE: mobile styling characters view

--- apps/web/src/components/Characters/CharacterSummaryTile.tsx
+++ apps/web/src/components/Characters/CharacterSummaryTile.tsx
@@ -27,138 +27,147 @@ const CharacterSummaryTile = ({
 	const navigate = useNavigate();
 	return (
 		<li className='list-row text-neutral font-cabin cursor-pointer px-4 py-2'>
-			<div className='avatar avatar-placeholder flex flex-col items-center justify-center'>
+			<div className='avatar avatar-placeholder flex flex-col items-center justify-start md:justify-center'>
 				<div className='bg-tile font-grenze text-primary size-10 rounded-full'>
 					<span className='text-3xl'>{char.name.charAt(0)}</span>
 				</div>
 			</div>
-			<div className='flex flex-col items-start'>
-				<div
-					className={cn(
-						'text-primary flex flex-row items-baseline text-base font-semibold',
-						{
-							'text-neutral': char.isDead,
-						},
-					)}
-				>
-					{char.name}
-					{char.surname ? `, ${char.surname}` : ''}
-					{char.isDead && (
-						<GiTombstone className='icon-neutral dark:icon-neutral icon-base mr-2' />
-					)}
-					<div className='ml-4 flex flex-row items-baseline gap-2'>
-						<div className='text-neutral-content text-sm capitalize'>
-							{char.subspecies || ''} {char.species}
+			<div className='flex flex-col items-start justify-start md:flex-row'>
+				<div className='flex w-full flex-col items-start'>
+					{/* NAME */}
+					<div
+						className={cn(
+							'text-primary flex w-full flex-col items-start text-base font-semibold md:flex-row md:items-baseline',
+							{
+								'text-neutral': char.isDead,
+							},
+						)}
+					>
+						<div className='flex w-full flex-col items-start md:flex-row md:items-baseline'>
+							{' '}
+							{char.name}{' '}
+							{char.isDead && (
+								<GiTombstone className='icon-neutral dark:icon-neutral icon-base mr-2' />
+							)}
+							{char.surname ? `, ${char.surname}` : ''}
 						</div>
-						<div className='text-neutral-content text-sm'>
-							Lvl. {char.level}
-						</div>
-					</div>
-				</div>
-
-				<div className='flex flex-col'>
-					{(user?.id === char.owner || user?.role === 'ADMIN') && (
-						<div className='flex flex-row items-baseline'>
-							<span className='text-neutral-content text-xs'>
-								Wandering in{' '}
-							</span>
-							<div className='ml-2 flex flex-row gap-2'>
-								<select
-									aria-placeholder='Assign to campaign'
-									value={char.campaigns ? char.campaigns : 'Unassigned'}
-									className='font-cabin dark:text-primary dark:caret-primary peer-default:dark:text-neutral dark:bg-card min-h-fit w-fit rounded-md py-0 text-sm text-xs shadow-sm'
-									onChange={e => updateCampaign(char.id, e.target.value)}
-								>
-									<option disabled={false}>Unassigned</option>
-									{campaignOptions?.map(option => (
-										<option
-											className='bg-card font-cabin'
-											value={option.value}
-											key={option.value}
-										>
-											{option.label}
-										</option>
-									))}
-								</select>
+						<div className='flex w-full flex-row items-baseline justify-start gap-2'>
+							<div className='text-neutral-content text-sm capitalize'>
+								{char.subspecies || ''} {char.species}
+							</div>
+							<div className='text-neutral-content text-sm'>
+								Lvl. {char.level}
 							</div>
 						</div>
+					</div>
+					{/* CAMPAIGN */}
+					<div className='flex w-full flex-col'>
+						{(user?.id === char.owner || user?.role === 'ADMIN') && (
+							<div className='flex w-full flex-row items-start gap-1 md:flex-row md:items-baseline'>
+								<span className='text-neutral-content w-content text-xs'>
+									Wandering in{' '}
+								</span>
+								<div className='flex w-fit flex-row gap-2'>
+									<select
+										aria-placeholder='Assign to campaign'
+										value={char.campaigns ? char.campaigns : 'Unassigned'}
+										className='font-cabin dark:text-primary dark:caret-primary peer-default:dark:text-neutral dark:bg-card w-content min-h-fit rounded-md py-0 text-sm text-xs shadow-sm md:w-fit'
+										onChange={e => updateCampaign(char.id, e.target.value)}
+									>
+										<option disabled={false}>Unassigned</option>
+										{campaignOptions?.map(option => (
+											<option
+												className='bg-card font-cabin'
+												value={option.value}
+												key={option.value}
+											>
+												{option.label}
+											</option>
+										))}
+									</select>
+								</div>
+							</div>
+						)}
+					</div>
+					{/* EXPERIENCE */}
+					<div className='flex w-full flex-row items-center gap-2'>
+						<progress
+							className='progress progress-primary h-1 w-full md:w-1/2'
+							value={char.experience}
+							max={char.level * 100}
+						></progress>
+						{(user?.role === 'ADMIN' ||
+							char.creator === user?.id ||
+							char.owner === user?.id) && (
+							<>
+								<span className='text-xs'>{char.experience}</span>
+								<input
+									className='input-xs border-1 text-primary w-10 rounded-sm text-center'
+									type='number'
+									id={`exp-${char.id}`}
+								></input>
+
+								<button
+									onClick={() => {
+										const input = document.querySelector(
+											`#exp-${char.id}`,
+										) as HTMLInputElement;
+										console.log(input.value);
+
+										updateXp(input.valueAsNumber, char);
+										//input.value = '';
+									}}
+								>
+									<FiPlusCircle className='icon-primary-sm cursor-pointer' />
+								</button>
+							</>
+						)}
+					</div>
+				</div>
+				{/* HEALTH */}
+				<div className='hidden w-1/6 items-center justify-center gap-2 md:flex md:flex-col'>
+					<div className='font-cabin text-error flex flex-row'>
+						<GiGlassHeart className='icon-error dark:icon-error icon-base mr-2' />{' '}
+						{char?.health?.current}
+					</div>
+
+					{char.isCaster && (
+						<div className='font-cabin text-info flex flex-row'>
+							<GiPotionBall className='icon-info dark:icon-info icon-base mr-2' />{' '}
+							{char?.spirit?.current}
+						</div>
 					)}
 				</div>
-				<div className='flex w-full flex-row items-center gap-2'>
-					<progress
-						className='progress progress-primary h-1 w-full md:w-1/2'
-						value={char.experience}
-						max={char.level * 100}
-					></progress>
+				{/* ADMIN */}
+				<div className='hidden flex-row items-end justify-end gap-1 md:flex md:flex-col md:justify-start'>
 					{(user?.role === 'ADMIN' ||
 						char.creator === user?.id ||
 						char.owner === user?.id) && (
-						<>
-							<span className='text-xs'>{char.experience}</span>
-							<input
-								className='input-xs border-1 text-primary w-10 rounded-sm text-center'
-								type='number'
-								id={`exp-${char.id}`}
-							></input>
-
-							<button
-								onClick={() => {
-									const input = document.querySelector(
-										`#exp-${char.id}`,
-									) as HTMLInputElement;
-									console.log(input.value);
-
-									updateXp(input.valueAsNumber, char);
-									//input.value = '';
-								}}
-							>
-								<FiPlusCircle className='icon-primary-sm cursor-pointer' />
-							</button>
-						</>
+						<SmallCircleButton
+							color='bg-accent'
+							onClick={e => {
+								e.stopPropagation();
+								navigate({
+									to: '/characters/edit/$id',
+									params: { id: `${char?.id}` },
+								});
+							}}
+						>
+							<FiEdit className='icon-background md:icon-sm size-3' />
+						</SmallCircleButton>
+					)}
+					{user?.role === 'ADMIN' && (
+						<SmallCircleButton
+							color='bg-error'
+							onClick={e => {
+								e.stopPropagation();
+								handleDelete(char.id);
+							}}
+						>
+							<FiTrash2 className='icon-background md:icon-sm size-3' />
+						</SmallCircleButton>
 					)}
 				</div>
-			</div>
-			<div className='flex flex-col justify-center gap-2'>
-				<div className='font-cabin text-error flex flex-row'>
-					<GiGlassHeart className='icon-error dark:icon-error icon-base mr-2' />{' '}
-					{char?.health?.current}
-				</div>
-
-				{char.isCaster && (
-					<div className='font-cabin text-info flex flex-row'>
-						<GiPotionBall className='icon-info dark:icon-info icon-base mr-2' />{' '}
-						{char?.spirit?.current}
-					</div>
-				)}
-			</div>
-			<div className='flex flex-col items-center justify-start gap-1'>
-				{(user?.role === 'ADMIN' ||
-					char.creator === user?.id ||
-					char.owner === user?.id) && (
-					<SmallCircleButton
-						color='bg-accent'
-						onClick={e => {
-							e.stopPropagation();
-							navigate({
-								to: '/characters/edit/$id',
-								params: { id: `${char?.id}` },
-							});
-						}}
-					>
-						<FiEdit className='icon-stone-900-sm' />
-					</SmallCircleButton>
-				)}
-				{user?.role === 'ADMIN' && (
-					<SmallCircleButton
-						color='bg-error'
-						onClick={e => {
-							e.stopPropagation();
-							handleDelete(char.id);
-						}}
-					>
-						<FiTrash2 className='icon-background-sm' />
-					</SmallCircleButton>
-				)}
 			</div>
 		</li>
 	);
