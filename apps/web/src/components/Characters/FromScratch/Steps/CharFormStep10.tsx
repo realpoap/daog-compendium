64372ae6cc0397a2a2115e8589deec1b6c678396
realpoap <alapetite.mathieu@gmail.com>
2025-04-29 21:06:06 -0400

    📦 NEW: Select career component logic (needs skill + stat implementation)

--- apps/web/src/components/Characters/FromScratch/Steps/CharFormStep10.tsx
+++ apps/web/src/components/Characters/FromScratch/Steps/CharFormStep10.tsx
@@ -1,8 +1,122 @@
+import { Career, careersData } from '@/data/careers';
+import { useCharacterForm } from '@/store/characterContext';
+import { ChangeEvent, useEffect, useState } from 'react';
+
 const CharFormStep10 = () => {
+	const { methods } = useCharacterForm();
+	const [selectedCareer, setSelectedCareer] = useState<Career | null>();
+	const [selectedCareerType, setSelectedCareerType] = useState<string | null>();
+
+	useEffect(() => {
+		if (!selectedCareer) return;
+		methods.setValue('path.careers', [selectedCareer.ranks[0].name]);
+	}, [selectedCareer]);
+
+	const handleCareerTypeSelect = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.ariaLabel;
+		if (value === 'All') {
+			setSelectedCareerType(null);
+			setSelectedCareer(null);
+		}
+		setSelectedCareerType(value);
+	};
+	const handleCareerSelect = (id: number) => {
+		const career = careersData.find(c => c.id === id);
+		setSelectedCareer(career);
+	};
+
 	return (
-		<div>
-			<fieldset>
-				<span>Careers</span>
+		<div className='flex w-full flex-col items-center justify-start'>
+			<fieldset className='flex w-full flex-col items-center'>
+				<label className='fieldset-legend label font-cabin text-neutral-content mb-1 pb-0 text-xs capitalize'>
+					Careers
+				</label>
+
+				<div className='my-2 filter'>
+					<input
+						className='btn btn-xs filter-reset focus:btn-primary w-fit border-0'
+						type='radio'
+						name='type'
+						aria-label='All'
+						onChange={e => handleCareerTypeSelect(e)}
+					/>
+					<input
+						className='btn btn-xs focus:btn-primary border-0'
+						type='radio'
+						name='type'
+						aria-label='Craft'
+						onChange={e => handleCareerTypeSelect(e)}
+					/>
+					<input
+						className='btn btn-xs focus:btn-primary border-0'
+						type='radio'
+						name='type'
+						aria-label='Arts & Letters'
+						onChange={e => handleCareerTypeSelect(e)}
+					/>
+					<input
+						className='btn btn-xs focus:btn-primary border-0'
+						type='radio'
+						name='type'
+						aria-label='Gathering'
+						onChange={e => handleCareerTypeSelect(e)}
+					/>
+					<input
+						className='btn btn-xs focus:btn-primary border-0'
+						type='radio'
+						name='type'
+						aria-label='Service'
+						onChange={e => handleCareerTypeSelect(e)}
+					/>
+					<input
+						className='btn btn-xs focus:btn-primary border-0'
+						type='radio'
+						name='type'
+						aria-label='Spiritual'
+						onChange={e => handleCareerTypeSelect(e)}
+					/>
+				</div>
+				<div className='flex w-full flex-col gap-4 sm:flex-row'>
+					<div className='h-50 w-1/2 overflow-y-scroll rounded-sm shadow'>
+						{careersData
+							.filter(c => {
+								if (selectedCareerType) {
+									return c.type === selectedCareerType;
+								} else return true;
+							})
+							.sort(function (a, b) {
+								return a.name.localeCompare(b.name);
+							})
+							.map(option => (
+								<div
+									key={`${option.name}-choice`}
+									onClick={() => handleCareerSelect(option.id)}
+									className='hover:text-primary cursor-pointer rounded-sm pl-2'
+								>
+									{option.name}
+								</div>
+							))}
+					</div>
+					{selectedCareer && (
+						<div className='font-cabin bg-tile h-50 flex w-1/2 flex-col gap-2 rounded-lg p-4 shadow-sm'>
+							<h3 className='font-grenze text-xl'>{selectedCareer.name}</h3>
+							<p className='text-sm italic'>{selectedCareer.type}</p>
+							<div className='flex flex-col items-start'>
+								{selectedCareer.ranks.map((r, i) => (
+									<div
+										key={r.name}
+										className='flex flex-row items-center gap-2'
+									>
+										<span className='badge badge-xs badge-primary'>
+											{i + 1}
+										</span>
+										<h4>{r.name}</h4>
+									</div>
+								))}
+							</div>
+						</div>
+					)}
+				</div>
 			</fieldset>
 		</div>
 	);
--- apps/web/src/components/Characters/FromScratch/Steps/CharFormStep9.tsx
+++ apps/web/src/components/Characters/FromScratch/Steps/CharFormStep9.tsx
@@ -1,19 +1,143 @@
-import { Field, Select } from '@/components/RHFComponents';
+import { Field, InputNumber, Select } from '@/components/RHFComponents';
+import { useCharacterForm } from '@/store/characterContext';
 
 const CharFormStep9 = () => {
+	const { methods } = useCharacterForm();
+	const tree = methods.watch('path.tree');
+
 	return (
 		<div>
 			<fieldset>
 				<Field
-					name='path.talentTree'
+					name='path.tree'
 					label='Talent tree'
 				>
 					<Select
-						name='path.talentTree'
+						name='path.tree'
 						options={['Adroitness', 'Constitution', 'Perception', 'Shroudness']}
 						defaultValue=''
 					/>
 				</Field>
+				{tree === 'Adroitness' && (
+					<section className='container mb-6 flex flex-col items-center justify-center md:w-1/2'>
+						<h4 className='font-grenze text-xl font-semibold tracking-wider text-purple-300'>
+							Adroitness
+						</h4>
+						<div className='flex flex-row justify-center gap-2'>
+							<Field
+								name='profile.statsStarting.CEL'
+								width='small'
+								label='CEL'
+							>
+								<InputNumber name='profile.statsStarting.CEL' />
+							</Field>
+							<Field
+								name='profile.statsStarting.AGI'
+								width='small'
+								label='AGI'
+							>
+								<InputNumber name='profile.statsStarting.AGI' />
+							</Field>
+							<Field
+								name='profile.statsStarting.DEX'
+								width='small'
+								label='DEX'
+							>
+								<InputNumber name='profile.statsStarting.DEX' />
+							</Field>
+						</div>
+					</section>
+				)}
+				{tree === 'Constitution' && (
+					<section className='container mb-6 flex flex-col items-center justify-center md:w-1/2'>
+						<h4 className='font-grenze text-xl font-semibold tracking-wider text-purple-300'>
+							Constitution
+						</h4>
+						<div className='flex flex-row justify-center gap-2'>
+							<Field
+								name='profile.statsStarting.STR'
+								width='small'
+								label='STR'
+							>
+								<InputNumber name='profile.statsStarting.STR' />
+							</Field>
+							<Field
+								name='profile.statsStarting.END'
+								width='small'
+								label='END'
+							>
+								<InputNumber name='profile.statsStarting.END' />
+							</Field>
+							<Field
+								name='profile.statsStarting.VIT'
+								width='small'
+								label='VIT'
+							>
+								<InputNumber name='profile.statsStarting.VIT' />
+							</Field>
+						</div>
+					</section>
+				)}
+				{tree === 'Perception' && (
+					<section className='container mb-6 flex flex-col items-center justify-center md:w-1/2'>
+						<h4 className='font-grenze text-xl font-semibold tracking-wider text-purple-300'>
+							Perception
+						</h4>
+						<div className='flex flex-row justify-center gap-2'>
+							<Field
+								name='profile.statsStarting.WIL'
+								width='small'
+								label='WIL'
+							>
+								<InputNumber name='profile.statsStarting.WIL' />
+							</Field>
+							<Field
+								name='profile.statsStarting.INS'
+								width='small'
+								label='INS'
+							>
+								<InputNumber name='profile.statsStarting.INS' />
+							</Field>
+							<Field
+								name='profile.statsStarting.SEN'
+								width='small'
+								label='SEN'
+							>
+								<InputNumber name='profile.statsStarting.SEN' />
+							</Field>
+						</div>
+					</section>
+				)}
+				{tree === 'Shroudness' && (
+					<section className='container mb-6 flex flex-col items-center justify-center md:w-1/2'>
+						<h4 className='font-grenze text-xl font-semibold tracking-wider text-purple-300'>
+							Shroudness
+						</h4>
+						<div className='flex flex-row justify-center gap-2'>
+							<Field
+								name='profile.statsStarting.CHA'
+								width='small'
+								label='CHA'
+							>
+								<InputNumber name='profile.statsStarting.CHA' />
+							</Field>
+							<Field
+								name='profile.statsStarting.SOC'
+								width='small'
+								label='SOC'
+							>
+								<InputNumber name='profile.statsStarting.SOC' />
+							</Field>
+							<Field
+								name='profile.statsStarting.ERU'
+								width='small'
+								label='ERU'
+							>
+								<InputNumber name='profile.statsStarting.ERU' />
+							</Field>
+						</div>
+					</section>
+				)}
 			</fieldset>
 		</div>
 	);
